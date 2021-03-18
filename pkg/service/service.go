39f9c3663aa85f641556d7e6296310d464f2c1d8
bqxtt <1486126243@qq.com>
2021-03-19 01:25:56 +0800

    crawler update

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ecnuvj/vhoj_common/pkg/common/constants/remote_oj"
 	"github.com/ecnuvj/vhoj_crawler/pkg/holder"
 	"github.com/ecnuvj/vhoj_db/pkg/dao/mapper/problem_mapper"
+	"github.com/ecnuvj/vhoj_db/pkg/dao/model"
 )
 
 type CrawlerService struct {
@@ -23,6 +24,27 @@ func (CrawlerService) CrawlProblem(remoteOJ remote_oj.RemoteOJ, problemId string
 	if err != nil {
 		return err
 	}
+	if rawProblem.ID == 0 {
+		fmt.Println("fuck")
+	}
+
 	//todo group problem
+	_, err = problem_mapper.ProblemMapper.AddOrModifyProblemGroup(&model.ProblemGroup{
+		RawProblemId:    rawProblem.ID,
+		GroupId:         rawProblem.ID,
+		MainProblem:     true,
+		RemoteOJ:        remote_oj.HDU,
+		RemoteProblemId: problemId,
+	})
+	if err != nil {
+		fmt.Printf("problem group error: %v", err)
+	}
+	_, _ = problem_mapper.ProblemMapper.AddOrModifyProblem(&model.Problem{
+		GroupId:      rawProblem.ID,
+		RawProblemId: rawProblem.ID,
+	})
+	if err != nil {
+		fmt.Printf("problem error: %v", err)
+	}
 	return nil
 }
--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"github.com/ecnuvj/vhoj_common/pkg/common/constants/remote_oj"
 	"github.com/ecnuvj/vhoj_crawler/pkg/bootstrap/database"
+	"strconv"
 	"testing"
 )
 
 func TestCrawlerService_CrawlProblem(t *testing.T) {
 	database.Init()
 	service := &CrawlerService{}
-	err := service.CrawlProblem(remote_oj.HDU, "1050")
-	if err != nil {
-		fmt.Printf("err: %v", err)
-		return
+	for i := 1000; i < 1050; i++ {
+		err := service.CrawlProblem(remote_oj.HDU, strconv.Itoa(i))
+		if err != nil {
+			fmt.Printf("%v err: %v\n", i, err)
+		}
 	}
+	//err := service.CrawlProblem(remote_oj.HDU, "1000")
+	//if err != nil {
+	//	fmt.Printf("err: %v\n", err)
+	//}
 }
